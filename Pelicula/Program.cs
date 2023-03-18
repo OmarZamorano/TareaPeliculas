0ed1f64c2ff2fb35c2d8c358f7f031ac6c8d8475
OmarZamorano <126824615+OmarZamorano@users.noreply.github.com>
2023-03-18 22:33:47 +0000

    modified:   Pelicula/Program.cs

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -6,11 +6,10 @@ namespace Pelicula
 
     public class Pelicula
     {
-        //Campos
-        public string titulo;
-        public short año;
-        public string pais;
-        public string director;
+        private string titulo;
+        private short año;
+        private string pais;
+        private string director;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
@@ -18,15 +17,34 @@ namespace Pelicula
         {
             this.titulo=t; this.año=a;
         }
+        public Pelicula()
+        {
+
+        }
+
         //Métodos
-     
         public void Imprime()
         {
             Console.WriteLine($"{titulo} ({año})");
 
         }
 
-
+        public void setTitulo(string t)
+        {
+            t=titulo;
+        }
+        public string getTitulo()
+        {
+            return titulo;
+        }
+        public void setAño(short a)
+        {
+            a=año;
+        }
+        public short getAño()
+        {
+            return año;
+        }
     }
 
     public class Actor
@@ -50,11 +68,14 @@ namespace Pelicula
     {
         static void Main(string[] args)
         {
-
-            Pelicula p1=new Pelicula("Interestelar",2014);
-            Pelicula p2=new Pelicula("Top Gun",2022);
-            p1.Imprime();
-            p1.Imprime();
+            Pelicula p1=new Pelicula();
+            Pelicula p2=new Pelicula();
+            p1.setTitulo("Interestelar");
+            p1.setAño(2014);
+            Console.WriteLine("{0}({1})", p1.getTitulo(), p1.getAño());
+            p2.setTitulo("Top Gun");
+            p2.setAño(2022);
+            Console.WriteLine("{0}({1})", p2.getTitulo(), p2.getAño());
             //Console.WriteLine("Hello");
         }
     }
