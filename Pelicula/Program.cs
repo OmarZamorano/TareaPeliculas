3089d6088190402f5532f5c5c8b591e41da7a5f5
OmarZamorano <126824615+OmarZamorano@users.noreply.github.com>
2023-03-20 03:54:34 +0000

    modified:   Pelicula/Program.cs

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -6,58 +6,70 @@ namespace Pelicula
 
     public class Pelicula
     {
-        private string titulo;
+        //Campos
         private short año;
-        private string pais;
+        private string país;
         private string director;
+        private string titulo;
         private List<Actor> actores = new List<Actor>();
 
-        //Constructores
-        public Pelicula(string t,short a)
-        {
-            this.titulo=t; this.año=a;
-        }
         public Pelicula()
         {
-
         }
-
+        public Pelicula(string t, short a)
+        {
+            this.titulo = t;
+            this.año = a;
+        }
         //Métodos
+        public void SetTitulo(string t)
+        {
+            this.titulo = t;
+        
+        }
+        public void SetAño(short a)
+        {
+            this.año = a;
+        }
+        public string GetTitulo()
+        {
+            return this.titulo;
+        }
+        public short GetAño()
+        {
+            return this.año;
+        }
+        public void AgregaActor(Actor a)
+        {
+            actores.Add(a);
+        }
         public void Imprime()
         {
             Console.WriteLine($"{titulo} ({año})");
-
         }
-
-        public void setTitulo(string t)
+        public void ImprimeActores()
         {
-            t=titulo;
-        }
-        public string getTitulo()
-        {
-            return titulo;
-        }
-        public void setAño(short a)
-        {
-            a=año;
-        }
-        public short getAño()
-        {
-            return año;
+            foreach (Actor actor in actores)
+            {
+                actor.Imprime();
+            }
         }
     }
 
     public class Actor
     {
         //Propiedades
-
-        //Constructores
-
-
+        private string nombre;
+        private short año;
+        public Actor(string n, short a)
+        {
+            this.nombre = n;
+            this.año = a;
+        }
         //Métodos 
         public void Imprime()
         {
-            //Console.WriteLine($"{Nombre} ({Año})");
+            Console.WriteLine($"{nombre} ({año})");
         }
     }
 
@@ -68,15 +80,20 @@ namespace Pelicula
     {
         static void Main(string[] args)
         {
-            Pelicula p1=new Pelicula();
-            Pelicula p2=new Pelicula();
-            p1.setTitulo("Interestelar");
-            p1.setAño(2014);
-            Console.WriteLine("{0}({1})", p1.getTitulo(), p1.getAño());
-            p2.setTitulo("Top Gun");
-            p2.setAño(2022);
-            Console.WriteLine("{0}({1})", p2.getTitulo(), p2.getAño());
-            //Console.WriteLine("Hello");
+            Pelicula p1 = new Pelicula();
+            p1.SetTitulo("Top Gun");
+            p1.SetAño(2022);
+            p1.AgregaActor(new Actor("Tom Cruise", 1962));
+            p1.AgregaActor(new Actor("Miles Teller", 1987));
+            p1.Imprime();
+            p1.ImprimeActores();
+            Pelicula p2 = new Pelicula();
+            p2.SetTitulo("Interestelar");
+            p2.SetAño(2014);
+            p2.AgregaActor(new Actor("Matthew McConaughey", 1969));
+            p2.AgregaActor(new Actor("Jessica Chastain", 1977));
+            p2.Imprime();
+            p2.ImprimeActores();
         }
     }
 }
